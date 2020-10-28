d19c456cf8217dce185049d8747b60915889dad8
Emily <qtan20@stuy.edu>
2020-10-28 00:19:28 -0400

    I have tested this code and it works

--- BankAccount.java
+++ BankAccount.java
@@ -3,8 +3,8 @@ public class BankAccount{
   private int accountID;
   private String password;
 
-  public BankAccount(double b, int a, String p){
-    balance = b;
+  public BankAccount(int a, String p){
+    balance = 0;
     accountID = a;
     password = p;
   }
@@ -30,7 +30,7 @@ public class BankAccount{
   }
 
   public boolean withdraw(double amount){
-    if(amount >= 0 && amount < balance){
+    if(amount >= 0 && amount <= balance){
       balance -= amount;
       return true;
     }
