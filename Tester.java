ee1655d47e37102064c8a64436cfca272c2200bd
Emily <qtan20@stuy.edu>
2020-10-28 00:05:22 -0400

    I have tested this code and it works

--- Tester.java
+++ Tester.java
@@ -1,7 +1,9 @@
 public class Tester{
   public static void main(String[] args){
     BankAccount a1 = new BankAccount(0, 12345, "hello");
-    System.out.println(a1.deposit(10));
+    System.out.println(a1.deposit(100));
+    System.out.println(a1.getBalance());
+    System.out.println(a1.withdraw(200));
     System.out.println(a1.getBalance());
   }
 }
